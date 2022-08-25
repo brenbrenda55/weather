0c5f854ee124c3a60ff5aa7e69c836eef83abb43
brenbrenda55 <108436173+brenbrenda55@users.noreply.github.com>
2022-08-24 22:29:52 -0500

    geo locations

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # weather
+
+## title
+
+chalenge 6 -generating a current and 5 day forcast for any given city.
+
+## project description 
+
+* I have a weather dashboard with form inputs. When search for a city, you are presented with current and future conditions for that city and that city is added to the search history.
+* When you view current weather conditions for that city you are presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index.
+* When you view the UV index you are presented with a color that indicates whether the conditions are favorable, moderate, or severe.
+* When you view future weather conditions for that city you presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity.
+* When you click on a city in the search history you presented with current and future conditions for that city
+
+## url link for live website
+
+https://brenbrenda55.github.io/weather/
+
+## screenshot of final product
+
--- assests/js/home.js
+++ assests/js/home.js
@@ -7,7 +7,7 @@
 var searchformEl = document.getElementById("searchbtn"); console.log(searchbtn)
 var cityEl = document.getElementById("city")
 var pastSearchesEl = document.getElementById("pastSearches")
-var inputEl =document.querySelector("#input"); 
+var inputEl = document.querySelector("#input");
 var tempEl = document.querySelector("#temp");
 var windEl = document.querySelector("wind");
 var uvIndexEl = document.querySelector("#uv-index");
@@ -18,25 +18,29 @@ var uvIndexEl = document.querySelector("#uv-index");
 
 
 // geo code for long/lat
-function coordinations () {
-    var apiKey = 'bf38ae612140444abef29b253d307bfa';
-    var apiUrlG ='api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={apiKey}';
+function coordinations() {
+    // var apiKey = 'bf38ae612140444abef29b253d307bfa';
+    //var apiUrlG ='api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={apiKey}';
     window.addEventListener("load", () => {
         let long;
         let lat;
 
-        if (navigator.geolocation){
+        if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((position) => {
                 console.log(position)
                 long = position.coords.longitude;
                 lat = position.coords.longitude;
                 fetch(apiUrlG)
-                    .then((response) =>(
+                    .then((response) => (
                         response.json()
-                    ))})}})}
+                    ))
+            })
+        }
+    })
+}
 
 //var {temp,wind,uvindex} = data.currentForecast (city)
-tempEl.textContent = temp;
+//tempEl.textContent = temp;
 //windEl.textContent = wind;
 //uvIndexEl.textContent = uv-index;
 
@@ -45,93 +49,85 @@ tempEl.textContent = temp;
 // function for current forecast
 function currentForecast(city) {
     var apiKey = 'bf38ae612140444abef29b253d307bfa';
-    var apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
+    var apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
     console.log(apiUrl)
-     fetch(apiUrl)
-    .then(function(response) {
-       if (response.ok) {
-           response.json().then(function(data){
-               console.log(data);
-           });
-       } else {
-           alert('Error: Weather Not Found');
-       }
-   })
-   .catch(function(error) {
-       alert("Unable to display weather");
-   });
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                    // TODO: Second api call needs to go here
+                    
+                    // Checking for empty data
+                    if (!data.length) {
+                        return null;
+                    }
+                    
+                    const lon = data[0].lon;
+                    const lat = data[0].lat;
+                    
+                    fiveday(city, lat, lon);
+                });
+            } else {
+                alert('Error: Weather Not Found');
+            }
+        })
+        .catch(function (error) {
+            alert("Unable to display weather");
+        });
 }
 
 
 
 // function for 5 day forecast forecast
-function fiveday(city) {
+function fiveday(city, lat, lon) {
     var apiKey = 'bf38ae612140444abef29b253d307bfa';
-    var apiUrl = `api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${apiKey}`
+    var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
     console.log(apiUrl)
-     fetch(apiUrl)
-    .then(function(response) {
-       if (response.ok) {
-           response.json().then(function(data){
-               console.log(data);
-           });
-       } else {
-           alert('Error: Weather Not Found');
-       }
-   })
-   .catch(function(error) {
-       alert("Unable to display weather");
-   });
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                   
+                    displayWeaterData(data);
+                    console.log(data);
+                });
+            } else {
+                alert('Error: Weather Not Found');
+            }
+        })
+        .catch(function (error) {
+            alert("Unable to display weather");
+        });
 }
 
+function displayWeaterData(data) {
+    // TODO: Display weather forecares
+    // document.createElement - create the elements you want in the page
+    // document.querySelector - selecting where you want to put the elements in the page
+    // for loop - looping over the weather forcast
+    // appendChild - add the elements in the page
 
-//5 day forcast
-// var fiveDay = function () {
-    //var apiUrl = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={53dfc2bbab4daa2b652b8ca5d214155b}"
-     
+}
 
-   // fetch(apiUrl)
-   // .then(function(response) {
-        //request was successful
-      //  if (response.ok) {
-     //       console.log(response);
-   ///         response.json().then(function(data){
- //           //    console.log(data);
-          //  });
-        //} else {
-        //    alert('Error: Weather Not Found');
-      //  }
-    //})
-    //.catch(function(error) {
-   //     alert("Unable to display weather");
-  // });
-//};
-
-
-
-// function for current forecast
-//function currentForecast(city) {
-    //var apiKey = 'bf38ae612140444abef29b253d307bfa';
-   // var apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
-    //console.log(apiUrl)
 
 
 
 //save city 
-var pastSearchesEl = function (){
-    localStorage.setItem(city,data);
+var pastSearchesEl = function () {
+    localStorage.setItem(city, data);
     JSON.Stringify(data)
 
 };
 
 
-function searchCity (event) { 
+function searchCity(event) {
     event.preventDefault()
-    currentForecast (inputEl.value)
+    currentForecast(inputEl.value)
 }
 
 
 // when search button is clicked 
-searchformEl.addEventListener("click",searchCity);
+searchformEl.addEventListener("click", searchCity);
 
 
